6c620ad6aaa629d8b9a1b6f3ece7ed63358bc32c
Matt Young <young0482@algonquinlive.com>
2015-03-26 19:09:08 -0400

    preetty mcuh done!

--- MidtermContacts/www/js/operator.js
+++ MidtermContacts/www/js/operator.js
@@ -4,9 +4,6 @@
 var loadCount = 0;
 var contactsList = [];
 var mapOptions = {};
-var long;
-var latt;
-var currentCon;
 
 
 //WAIT FOR BOTH DOMCONTENT AND DEVICE TO BE READY THEN START
@@ -32,14 +29,15 @@ document.addEventListener("deviceready",function(){
 
 function start(){
     
-    
+    //start the readyMap function
+    readyMap();
     
     //Set the hammer listener for tap on the modal window back btn
-   var backBtn = document.getElementById("divClose");
+   var backBtn = document.getElementById("svgClose");
    var ht = new Hammer(backBtn);
    ht.on('tap', hideModal);
     
-    var exitMap = document.getElementById("closeMap");
+    var exitMap = document.getElementById("svgBack");
    var ht2 = new Hammer(exitMap);
    ht2.on('tap', hideMap);
     
@@ -78,13 +76,49 @@ function start(){
 
 function successF(contacts){
     
-    console.log(contacts);
     
     //collect 12 contacts from phone and save to global object array
     for (var i=0;i<12;i++){
         
+        //container to hold the numbers object
+        var numCollect = [];
+        
+        //collect all sets of phone numbers for the contact
+        for (var y=0;y<contacts[i].phoneNumbers.length;y++){
+             
+            //get this numbers type and value
+            var newType = (contacts[i].phoneNumbers[y].type);
+            var newNum = (contacts[i].phoneNumbers[y].value);
+            
+            //check to see what type and create object accordingly
+            if (newType === "mobile"){
+                
+                var newObj = {mobile:newNum};
+                
+            }else if (newType === "home"){
+                
+                 var newObj = {home:newNum};
+                
+            }else if (newType === "work"){
+                
+                 var newObj = {work:newNum};
+                
+            }else{
+                
+                 var newObj = {other:newNum};
+            };
+            
+            //add number object to container.
+            numCollect.push(newObj);
+            
+        };
+        
+        
         //fetch new contact from phone's contacts object array
-        var newContact = {id:(contacts[i].id), name:(contacts[i].name.formatted), numbers:(contacts[i].phoneNumbers[0].value), lat:"",lng:""};
+        var newContact = {id:(contacts[i].id), 
+                          name:(contacts[i].name.formatted), 
+                          numbers:(numCollect), //using the numbers container
+                          lat:"",lng:""};
                         
         //push new contact object onto working array list.
         contactsList.push(newContact); 
@@ -150,22 +184,30 @@ function showConList(){
 ************************************/
 
 function showModal(ev){
-        alert("hi");
 
         //ev.stopPropagation();
         var nmt = document.getElementById("conName");
         var nLst = document.getElementById("numList");
-        var li2 = document.createElement("li");
-
+    
+        //display name
         nmt.innerHTML = contactsList[ev.target.id].name;
-        li2.innerHTML = contactsList[ev.target.id].numbers;
-
-        //clear previous list and append new numbers
+        //clear previous list of numbers
         nLst.innerHTML = "";
-        nLst.appendChild(li2);
-
-        console.log(ev);
-
+    
+        //loop to add all contacts numbers to list
+        for (i=0;i<contactsList[ev.target.id].numbers.length;i++){
+            
+            var li2 = document.createElement("li");
+            console.log(JSON.stringify(contactsList[ev.target.id]));
+            
+            for (prop in contactsList[ev.target.id].numbers[i]) {
+                li2.innerHTML = prop + ": " + (contactsList[ev.target.id].numbers[i][prop]);
+            }
+            
+            nLst.appendChild(li2);
+        }
+        
+        //show the modal
         document.querySelector("[data-role=modal]").style.display="block";
         document.querySelector("[data-role=overlay]").style.display="block";
 
@@ -175,80 +217,106 @@ function hideModal(ev){
        // ev.stopPropagation();
 
         console.log(ev);
-        alert("hideen");
 
-        
         document.querySelector("[data-role=modal]").style.display="none";
         document.querySelector("[data-role=overlay]").style.display="none";
 
 };
+
+
+
+
 
 /*******************************************************
         DYNAMIC MAP FUNCTIONS
 **********************************************************/
 
 var map;
+var pos;
+var long;
+var latt;
+var currentCon;
+var oneMark;
+var markerCheck;
+var marker;
+var infoWindow;
 
-function showMap(ev) {
+
+function showMap(ev){
     
-    
-  var mapOptions = {
-      disableDoubleClickZoom: true,
-    zoom: 8
-  };
-    
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  // Try HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
-
-      var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: pos,
-        content: 'Location found using HTML5.'
-      });
-
-      map.setCenter(pos);
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-  }
+    oneMark = true;
+    //clear previous marker
+    if (marker){
+        marker.setMap(null);
+    }
     
     //Show the Map
     document.querySelector("[data-role=page]").style.display="none";
     document.querySelector("[data-role=map]").style.display="block";
-    
-    
-    
-    //Check to see if contact Location needs to be set or not
-    if (contactsList[ev.target.id].lat === "" && contactsList[ev.target.id].lng === ""){
-        
-        google.maps.event.addListener(map, 'dblclick', function(event){
-        
-            //save the ev target Id for further use
-            currentCon = ev.target.id;
-            placeMarker(event.latLng);
+
+        //save the ev target Id for further use
+        currentCon = ev.target.id;
+
+        //Check to see if contact Location needs to be set or not
+        if (contactsList[ev.target.id].lat === "" && contactsList[ev.target.id].lng === ""){
             
-        });
+                //alert user to double click
+                alert("Please double tap on this contacts location.");
+                //Add the listener for location set on Doubleclick.
+                google.maps.event.addListener(map, 'dblclick', function(event){
+                        
+                        //set marker check to true for animations sake
+                        markerCheck = true;
+                        
+                        placeMarker(event.latLng);
+
+                });
+
+            
+        }else{
         
-        alert("To add " + ev.target.innerHTML + " location, Please double tap on the map.");
-    }else{
-        
-        var myLatlng = new google.maps.LatLng( contactsList[ev.target.id].lat , contactsList[ev.target.id].lng );
-        
-         placeMarker(myLatlng);
-    }
+
+                //set marker check to false to add animations
+                markerCheck = false;
+                
+            
+                var myLatlng = new google.maps.LatLng( contactsList[ev.target.id].lat , contactsList[ev.target.id].lng );
+            
+                placeMarker(myLatlng);
+        };
     
-    console.log(ev);
     
+};
+
+function readyMap() {
     
+    //map options
+        var mapOptions = {
+            disableDoubleClickZoom: true,
+            zoom: 8
+        };
+
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
+
+        // Try HTML5 geolocation
+        if(navigator.geolocation) {
+            
+                navigator.geolocation.getCurrentPosition(function(position) { //success function
+                
+                    pos = new google.maps.LatLng( position.coords.latitude, position.coords.longitude );
+                   
+                    map.setCenter(pos);
+                    
+                }, function() { //geolocation failure function
+                    
+                        handleNoGeolocation(true);
+                });
+            
+        } else {
+            // Browser doesn't support Geolocation
+            handleNoGeolocation(false);
+        }
 
 };
 
@@ -256,25 +324,47 @@ function showMap(ev) {
 
 function placeMarker(location) {
     
-        var marker = new google.maps.Marker({
-          position: location,
-          map: map
-        });
+    if (oneMark){
+        
+        oneMark = false;
+        
+        if (markerCheck){
 
-        contactsList[currentCon].lat = marker.getPosition().lat();
-        contactsList[currentCon].lng = marker.getPosition().lng();
+                marker = new google.maps.Marker({
+                  position: location,
+                  map: map
+                });
+
+                contactsList[currentCon].lat = marker.getPosition().lat();
+                contactsList[currentCon].lng = marker.getPosition().lng();
+
+                //update the local storage
+                localStorage.setItem("contacts-youn0482", JSON.stringify(contactsList) );
+
+                map.setCenter(location);
+            
+              
+        } else {
+            
+                marker = new google.maps.Marker({
+                        position: location,
+                        animation: google.maps.Animation.BOUNCE,
+                        map: map
+                });
+                
+           
+             map.setCenter(location);
+                
+        };
+        
+    };
     
-        console.log(contactsList[currentCon].lat);
-        console.log(contactsList[currentCon].lng); 
-
-        map.setCenter(location);
-}
+};
 
 function hideMap(ev){
        // ev.stopPropagation();
 
-        console.log(ev);
-        alert("hideen");
+        
 
         
     document.querySelector("[data-role=page]").style.display="block";
